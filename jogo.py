ea50f149dbe7007530fbd2e837de23c6b263d717
Eduardo Machado <eduardomachado@MacBook-Air-de-Eduardo.local>
2025-01-21 21:41:54 -0300

    Random

--- jogo.py
+++ jogo.py
@@ -3,14 +3,24 @@ print("Este é um protótipo de um jogo Jokenpo")
 print("Insira sua jogada, lembrando que deve ser uma das seguintes opções:")
 print("Pedra, Papel ou Tesoura")
 print("Boa sorte!")
-jogador2 = "Tesoura"
-print(jogador2)
-jogador1 = input("Digite sua jogada:")
-if jogador1 == 'Pedra':
-  print("Você ganhou!")
-elif jogador1 == 'Papel':
- print("Você perdeu!")
-elif jogador1 == 'Tesoura':
+import random
+opcoes = ["Pedra", "Papel", "Tesoura"]
+cpu = random.choice(opcoes)
+print(cpu)
+jogador = input("Digite sua jogada:")
+if cpu == jogador:
   print("Empate!")
+elif cpu == 'Pedra'and jogador == 'Papel':
+  print("Você ganhou!")
+elif cpu == 'Pedra'and jogador == 'Tesoura':
+  print("Você perdeu!")
+elif cpu == 'Papel'and jogador == 'Tesoura':
+  print("Você ganhou!")
+elif cpu == 'Papel'and jogador == 'Pedra':
+  print("Você perdeu!")
+elif cpu == 'Tesoura'and jogador == 'Pedra':
+  print("Você ganhou!")
+elif cpu == 'Tesoura'and jogador == 'Papel':
+  print("Você perdeu!")
 else:
- print("Opção inválida tente novamente!")
+    print("Opção inválida tente novamente!")
